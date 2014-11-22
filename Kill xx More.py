889388515221b756285244aeac8ddc083b5cd7d7
chayen <chayenjr.sk131@gmail.com>
2014-11-22 17:34:17 +0700

    init game

---
+++ Kill xx More.py
@@ -0,0 +1,26 @@
+import pygame
+from pygame.locals import *
+
+import GameScreen
+
+class Kill_xx_More_Game(GameScreen.GameScreen):
+	BLACK = pygame.Color('black')
+
+	def __init__(self):
+		super(Kill_xx_More_Game, self).__init__('Kill_xx_More', Kill_xx_More_Game.BLACK)
+
+	def init(self):
+		super(Kill_xx_More_Game, self).init()
+
+	def render(self, surface):
+		pass
+
+	def update(self):
+		pass
+
+def main():
+    game = Kill_xx_More_Game()
+    game.run()
+
+if __name__ == '__main__':
+    main()
