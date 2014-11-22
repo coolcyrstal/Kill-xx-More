84e3405e768368289093433f3e91568afb57631a
chayen <chayenjr.sk131@gmail.com>
2014-11-22 23:41:48 +0700

    game menu screen picture

--- GameScreen.py
+++ GameScreen.py
@@ -3,16 +3,13 @@ from pygame.locals import *
 
 class GameScreen(object):
 
-	def __init__(self, title, background_color, window_size = (1080,640), fps = 60):
+	def __init__(self, title, background_color, window_size = (1280,720), fps = 60):
 		self.title = title
 		self.window_size = window_size
 		self.fps = fps
 		self.background_color = background_color
-
 		self.is_terminated = False
-
-	def terminate(self):
-		self.is_terminated = True
+		self.BG = pygame.image.load("res/MenuScreen.png")
 
 	def __game_init(self):
 		pygame.init()
@@ -21,13 +18,23 @@ class GameScreen(object):
 		pygame.display.set_caption(self.title)
 		self.font = pygame.font.SysFont("monospace", 20)
 
+	def __handle_events(self):
+		for event in pygame.event.get():
+			if event.type == QUIT:
+				self.terminate()
+
+	def terminate(self):
+		self.is_terminated = True
+
 	def run(self):
 		self.init()
 		while not self.is_terminated:
+			self.__handle_events()
+
 			self.update()
 
 			self.surface.fill(self.background_color)
-			self.render(self.surface)
+			self.surface.blit(self.BG,(0,0))
 			pygame.display.update()
 
 			self.clock.tick(self.fps)
